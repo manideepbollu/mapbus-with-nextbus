1831e5748b3eabff3ca188ef651dad31cb478031
alwaysbollu <always.bollu@gmail.com>
2018-01-02 04:03:18 -0500

    bugfix - updated app container component

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from 'store/index';
-import App from 'containers/App';
+import App from './containers/App';
 
 const routes = (
   <ConnectedRouter history={history}>
